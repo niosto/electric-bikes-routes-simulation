b7f85d0dae673dda05beffdaaa0326bfe5f327a1
agr <agarcesr@eafit.edu.co>
2025-12-09 12:28:57 -0500

    fix

--- client/src/components/map/MapView.jsx
+++ client/src/components/map/MapView.jsx
@@ -15,7 +15,7 @@ import {
 import { COLORS } from "../../utils/colors.js";
 import { makeColoredIcon } from "../../utils/icons.js";
 import CoordsPanel from "./CoordsPanel.jsx";
-import VITE_API_URL from "../../.env"
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 // ================== CONFIG ICONS ==================
 delete L.Icon.Default.prototype._getIconUrl;
